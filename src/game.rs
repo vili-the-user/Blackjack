1e2f48a0ef782ab2a5e8e7691d0c51209534fe51
vilio <vili.onikki@outlook.com>
2024-10-12 16:42:52 +0300

    Added functions for creating new decks and shuffling them

--- src/game.rs
+++ src/game.rs
@@ -4,10 +4,52 @@ use std::io;
 use std::thread::sleep;
 use std::time::Duration;
 
+use rand::thread_rng;
+use rand::seq::SliceRandom;
+
 /// Player struct
 struct Player {
     name: String,
     wealth: u32
+}
+
+/// Card struct
+struct Card {
+    num: String,
+    icon: char
+}
+
+// Create constant arrays for card icons and numbers
+const ICON_ARRAY: [char; 4] = ['♠', '♣', '♥', '♦'];
+const NUM_ARRAY: [&str; 13] = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+
+/// Creates a sorted list of cards
+/// 
+/// # Returns
+/// 
+/// Vec of Card objects
+fn create_deck_vec() -> Vec<Card> {
+
+    // Create new deck vector
+    let mut deck_vec: Vec<Card> = Vec::new();
+
+    // Add one card of each type to the vector
+    for icon in ICON_ARRAY {
+        for num in NUM_ARRAY {
+
+            deck_vec.push(Card {
+                num: String::from(num),
+                icon: icon
+            });
+        }
+    }
+
+    return deck_vec;
+}
+
+/// Shuffles passed deck of cards
+fn shuffle_deck(deck_vec: &mut Vec<Card>) {
+    deck_vec.shuffle(&mut thread_rng());
 }
 
 /// Starts a game with new stats
@@ -38,6 +80,10 @@ pub fn new_game() {
 
 /// Main game loop
 fn game(player: &mut Player) {
+
+    // Create new deck
+    let mut deck = create_deck_vec();
+
     loop {
         println!("---");
         println!("You have {}$", player.wealth);
@@ -66,5 +112,8 @@ fn game(player: &mut Player) {
 
         // Remove bet from player's wealth
         player.wealth -= bet;
+
+        // Shuffle deck
+        shuffle_deck(&mut deck);
     }
 }
